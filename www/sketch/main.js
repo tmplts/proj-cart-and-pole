8b3dda73bbe8af9eb2ddf1b85ff84f32e91da22a
Mr Josh <mr-josh@sparklapse.com>
2022-11-03 16:30:58 +1100

    Initial commit
    Initial commit with p5 and matter embedded. Also includes types and
    references.

---
+++ www/sketch/main.js
@@ -0,0 +1,11 @@
+/// <reference path="./types/p5/global.d.ts" />
+/// <reference path="./types/matter-js/index.d.ts" />
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(200, 200, 100);
+}
